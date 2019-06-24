90f71c574868f3081d074bf91415e7b2f1c3c239
Grzegorz Kułaga <poczta@grzegorzkulaga.pl>
2019-06-24 16:02:19 +0200

    Prostokat

--- Program.cs
+++ Program.cs
@@ -8,8 +8,41 @@ namespace Petle_Wzorki
 {
     class Program
     {
+        const char CHAR = '*';
+        static void Star() => Console.Write(CHAR);
+        static void StarLn() => Console.WriteLine(CHAR);
+        static void Space() => Console.Write(" ");
+        static void SpaceLn() => Console.WriteLine(" ");
+        static void NewLine() => Console.WriteLine();
+
+        public static void Prostokat(int n, int m)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Star();
+            }
+            NewLine();
+
+            for(int j=1; j<m-1; j++)
+            {
+                Star();
+                for (int i = 1; i < n-1; i++)
+                    Space();
+
+                StarLn();
+            }
+
+            for(int i=0; i<n; i++)
+            {
+                Star();
+            }
+            NewLine();
+            
+        }
+
         static void Main(string[] args)
         {
+            Prostokat(5, 7);
         }
     }
 }
